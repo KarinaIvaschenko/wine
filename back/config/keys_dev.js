b6fcba74cb6b8332ea52e55338884ff1c017007d
KarinaIvaschenko <karinka2323@gmail.com>
2023-11-03 22:32:13 +0200

    mongo

--- back/config/keys_dev.js
+++ back/config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: `mongodb+srv://admin:admin@wine.vmggmg3.mongodb.net/?retryWrites=true&w=majority`,
+    mongoURI: `mongodb+srv://admin:admin@wine.vmggmg3.mongodb.net/test/?retryWrites=true&w=majority`,
     secretOrKey: "random very very secret string",
 };
